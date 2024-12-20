aa5dd28afef23ef3b7c9ab04a3b9826fe94240bf
Iva Raikova <IDRaykova22@codingburgas.bg>
2024-12-21 01:37:00 +0200

    Implement start page. Close #4

--- codeyapp/presentationLayer/mainwindow.cpp
+++ codeyapp/presentationLayer/mainwindow.cpp
@@ -8,11 +8,12 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
     QPixmap lampBackground(":/assets/images/topLampBackground.png");
     QPixmap linesBackground(":/assets/images/backgroundLines.png");
+    QPixmap backgroundText(":/assets/images/backgroundText.png");
     ui->label_lampBackground->setPixmap(lampBackground);
     ui->label_linesBackground->setPixmap(linesBackground);
+    ui->label_backgroundText->setPixmap(backgroundText);
 }
 
 
