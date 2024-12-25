c828230208a868689e7fd5516310484427a179b7
Iva Raikova <IDRaykova22@codingburgas.bg>
2024-12-26 01:28:54 +0200

    Add feedback section .Close #9

--- codeyweb/src/App.tsx
+++ codeyweb/src/App.tsx
@@ -30,7 +30,7 @@ export default function App() {
       </div>
 
       <div className="flex justify-center items-center py-48 font-body">
-    <img src='./src/assets/leftArrow.png' className='absolute mt-96 left-0'/>
+        <img src='./src/assets/leftArrow.png' className='absolute mt-96 left-0' />
         <div className="rounded-[30px] border-[1px] border-[#191A23] bg-white shadow-[0px_5px_0px_0px_#191A23] w-[25vw] h-[63vh] p-8 mx-10">
           <h2 className="text-[2.3vw] font-bold">Be a</h2>
           <h3 className="text-[2.3vw] font-bold text-[#d0d838]">Reader</h3>
@@ -44,7 +44,7 @@ export default function App() {
           </ul>
           <p className="mt-6 text-[1.3vw] underline text-[#000] cursor-pointer">And more!</p>
         </div>
-        <img src='./src/assets/rightArrow.png' className='absolute mt-2 right-0'/>
+        <img src='./src/assets/rightArrow.png' className='absolute mt-2 right-0' />
         <div className="rounded-[30px] border-[1px] border-[#191A23] bg-white shadow-[0px_5px_0px_0px_#191A23] w-[25vw] h-[63vh] p-8 mx-10">
           <h2 className="text-[2.3vw] font-bold">Be a</h2>
           <h3 className="text-[2.3vw] font-bold text-[#d0d838]">Writer</h3>
@@ -60,6 +60,32 @@ export default function App() {
         </div>
       </div>
 
+      <div className="flex flex-col items-center justify-center py-16 font-body">
+        <h1 className="text-center text-[2rem] font-bold leading-relaxed">
+          The Ultimate Platform For <span className="underline"> Discovering Stories</span> And <br />
+          <span className="underline">Sharing Your Creativity</span> With The World.
+        </h1>
+
+        <img
+          className="my-6 w-[18rem] select-none"
+          src="./src/assets/feedbackStars.png"
+        />
+
+        <img
+          className="mb-[-1rem] mt-[-2.5rem] w-[35rem] select-none"
+          src="./src/assets/laptop.png"
+          alt="Laptop"
+        />
+
+        <p className="text-center font-light text-[1.2rem] px-4 max-w-[45vw]">
+          We are creating a vibrant platform where creativity meets community. Discover amazing authors, dive into captivating stories, or share your own works with the world. Whether you're a reader seeking inspiration or a writer ready to shine, Codey is the perfect place to connect, create, and grow!
+        </p>
+
+        <button className="mt-10 bg-white text-black text-[1.5rem] font-bold px-12 py-4 rounded-full border border-black shadow-[0px_5px_0px_0px_#191A23] 
+        hover:shadow-none hover:scale-105 hover:bg-black hover:text-white transition-all duration-300 ease-in-out">
+          Download
+        </button>
+      </div>
     </>
   );
 }
