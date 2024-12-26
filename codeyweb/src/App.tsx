c37b69c80d25aa4d4b43ea58730e83e4087bc696
Iva Raikova <IDRaykova22@codingburgas.bg>
2024-12-26 23:23:18 +0200

    Add animations and fix download button. Close #10

--- codeyweb/src/App.tsx
+++ codeyweb/src/App.tsx
@@ -1,16 +1,19 @@
 import './App.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
 export default function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center">
         <img className="h-[46.5rem] w-[1500px] select-none mb-10" src="./src/assets/backgroundImage.png" alt="Background Image" />
-        <h1 className="text-[6.5vw] absolute mb-52 font-bold font-body text-transparent" style={{ WebkitTextStrokeWidth: '1.2px', WebkitTextStrokeColor: '#000', }}>Codey</h1>
-        <h1 className="absolute text-[1.1vw] mb-14 font-body">Online library that allows people to show their work with the world.</h1>
-        <h1 className="underline absolute mt-8 text-[1.5vw] font-bold font-body"><a href="https://github.com/codingburgas/10grade-christmas-luck-codey" target='_blank'>Check us out</a></h1>
+        <h1 data-aos="zoom-in" className="text-[6.5vw] absolute mb-52 font-bold font-body text-transparent" style={{ WebkitTextStrokeWidth: '1.2px', WebkitTextStrokeColor: '#000', }}>Codey</h1>
+        <h1 data-aos="zoom-in" className="absolute text-[1.1vw] mb-14 font-body">Online library that allows people to show their work with the world.</h1>
+        <h1 data-aos="zoom-in" className="underline absolute mt-8 text-[1.5vw] font-bold font-body"><a href="https://github.com/codingburgas/10grade-christmas-luck-codey" target='_blank'>Check us out</a></h1>
       </div>
       <img className="w-[100vw] h-[7vh] select-none" src="./src/assets/separator.png" alt="Separator" />
-      <div className="flex items-center justify-center h-[15vh] px-4 mt-40">
+      <div data-aos="fade-down" className="flex items-center justify-center h-[15vh] px-4 mt-40">
         <img
           className="h-[5vw] mx-4"
           src="./src/assets/stars.png"
@@ -30,8 +33,8 @@ export default function App() {
       </div>
 
       <div className="flex justify-center items-center py-48 font-body">
-        <img src='./src/assets/leftArrow.png' className='absolute mt-96 left-0' />
-        <div className="rounded-[30px] border-[1px] border-[#191A23] bg-white shadow-[0px_5px_0px_0px_#191A23] w-[25vw] h-[63vh] p-8 mx-10">
+        <img data-aos="fade-up-right" src='./src/assets/leftArrow.png' className='absolute mt-96 left-0' />
+        <div data-aos="fade-down-right" className="rounded-[30px] border-[1px] border-[#191A23] bg-white shadow-[0px_5px_0px_0px_#191A23] w-[25vw] h-[63vh] p-8 mx-10">
           <h2 className="text-[2.3vw] font-bold">Be a</h2>
           <h3 className="text-[2.3vw] font-bold text-[#d0d838]">Reader</h3>
           <p className="mt-6 text-[1.3vw]">
@@ -44,8 +47,8 @@ export default function App() {
           </ul>
           <p className="mt-6 text-[1.3vw] underline text-[#000] cursor-pointer">And more!</p>
         </div>
-        <img src='./src/assets/rightArrow.png' className='absolute mt-2 right-0' />
-        <div className="rounded-[30px] border-[1px] border-[#191A23] bg-white shadow-[0px_5px_0px_0px_#191A23] w-[25vw] h-[63vh] p-8 mx-10">
+        <img data-aos="fade-up-left" src='./src/assets/rightArrow.png' className='absolute mt-2 right-0' />
+        <div data-aos="fade-down-left" className="rounded-[30px] border-[1px] border-[#191A23] bg-white shadow-[0px_5px_0px_0px_#191A23] w-[25vw] h-[63vh] p-8 mx-10">
           <h2 className="text-[2.3vw] font-bold">Be a</h2>
           <h3 className="text-[2.3vw] font-bold text-[#d0d838]">Writer</h3>
           <p className="mt-6 text-[1.3vw]">
@@ -61,7 +64,7 @@ export default function App() {
       </div>
 
       <div className="flex flex-col items-center justify-center py-16 font-body">
-        <h1 className="text-center text-[2rem] font-bold leading-relaxed">
+        <h1 data-aos="fade-down" className="text-center text-[2rem] font-bold leading-relaxed">
           The Ultimate Platform For <span className="underline"> Discovering Stories</span> And <br />
           <span className="underline">Sharing Your Creativity</span> With The World.
         </h1>
@@ -69,21 +72,24 @@ export default function App() {
         <img
           className="my-4 mt-10 w-[18rem] select-none"
           src="./src/assets/feedbackStars.png"
+          data-aos="zoom-in-down"
         />
 
         <img
           className="mb-[-1rem] mt-[-2.5rem] w-[35rem] select-none"
           src="./src/assets/laptop.png"
+          data-aos="zoom-out-up"
         />
 
-        <p className="text-center font-light text-[1.2rem] px-4 max-w-[45vw]">
+        <p data-aos="zoom-in" className="text-center font-light text-[1.2rem] px-4 max-w-[45vw]">
           We are creating a vibrant platform where creativity meets community. Discover amazing authors, dive into captivating stories, or share your own works with the world. Whether you're a reader seeking inspiration or a writer ready to shine, Codey is the perfect place to connect, create, and grow!
         </p>
 
-        <button className="mt-10 bg-white text-black text-[1.5rem] font-bold px-12 py-4 rounded-full border border-black shadow-[0px_5px_0px_0px_#191A23] 
+        <a href="https://github.com/codingburgas/10grade-christmas-luck-codey"><button data-aos="fade-up" className="mt-10 bg-white text-black text-[1.5rem] font-bold px-12 py-4 rounded-full border border-black shadow-[0px_5px_0px_0px_#191A23] 
         hover:shadow-none hover:scale-105 hover:bg-black hover:text-white transition-all duration-300 ease-in-out">
           Download
         </button>
+        </a>
       </div>
     </>
   );
